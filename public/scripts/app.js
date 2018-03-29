1bfcdf7abf8d2aaeabb4e7061f208cc9a46896e8
vagrant <kennethracadio@hotmail.com>
2018-03-29 18:11:55 +0000

    Finish page structure

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,111 @@
-// document.getElementById("ex").innerHTML = "Hello again"
-
-// console.log("hello")
-
-// const newDiv = document.createElement("div");
-// const newContent = document.createTextNode("Hi there and greetings!");
-// newDiv.appendChild(newContent);
-
-// const newDiv2 = document.createElement("div");
-// const newContent2 = document.createTextNode("Hi there!");
-// newDiv2.appendChild(newContent2);
-
-// const newDiv3 = newDiv2.cloneNode(true)
-
-
-
-// const currentDiv = document.getElementById("ex");
-
-
-// insertAfter = (el, referenceNode) => {
-//     referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-// }
-
-// insertAfter(newDiv, currentDiv);
-
-// insertAfter(newDiv2, newDiv);
-
-// insertAfter(newDiv3, newDiv2);
-
-getCORS = (url, success) => {
-  const xhr = new XMLHttpRequest();
-  if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
-  xhr.open('GET', url);
-  xhr.onload = success;
-  xhr.send();
-  return xhr;
-}
+const section = document.querySelector('section');
 
 const allNewsUrl = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-let allNews;
 
-getCORS(allNewsUrl, (request) => {
-  const response = JSON.parse(request.currentTarget.response) || JSON.parse(request.target.responseText);
-  console.log(response);
-  allNews = response
-  console.log("allNews", allNews)
 
-  console.log("allNews[1]", allNews[1])
+getNews = () => {
+  return fetch(allNewsUrl)
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((json) => {
+      return json
+    })
+}
 
-  const story = `https://hacker-news.firebaseio.com/v0/item/${allNews[1]}.json?print=pretty`
-  console.log(story)
+getStory = (story) => {
+  const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
+  return fetch(storyUrl)
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((json) => {
+      return json
+    })
+}
 
-  getCORS(story, (request) => {
-      const response = JSON.parse(request.currentTarget.response) || JSON.parse(request.target.responseText);
-      console.log("story", response);
-  });
+createNode = (element) => {
+  return document.createElement(element)
+}
 
-});
+append = (parent, el) => {
+  return parent.appendChild(el);
+}
+
+timeDiff = (time) => {
+  const unix = Math.round(+new Date()/1000);
+  const diffMins = Math.floor((unix - time) / 60)
+  const diffHoursString = minsToHours(diffMins)
+  let stringDiff;
+  (diffMins > 60) ? stringDiff = diffHoursString : stringDiff = `${diffMins} minutes ago`;
+  return stringDiff
+}
+
+minsToHours = (mins) => {
+  let stringDiff;
+  (Math.floor(mins / 60) > 1) ? stringDiff = `${Math.floor(mins / 60)} hours ago` : stringDiff = `${Math.floor(mins / 60)} hour ago`;
+  return stringDiff
+}
+
+storiesArr = (count, result) => {
+  let stories = result.filter((i, index) => (index >= count && index < count + 30))
+  // why is this not affecting global var?
+  count += 30;
+  console.log('from func', count)
+  return stories
+}
+
+renderStory = (storyObj, i) => {
+  getStory(storyObj).then((result) => {
+    postedElapsed = timeDiff(result.time)
+    const byUrl = `https://news.ycombinator.com/user?id=${result.by}`
+    const timeUrl = `https://news.ycombinator.com/item?id=${result.id}`
+    const logInUrl = `https://news.ycombinator.com/hide?id=${result.id}&goto=news`
+    const commentsUrl = `https://news.ycombinator.com/item?id=${result.id}`
+    let p = createNode("p")
+    let div = createNode("div")
+    p.innerHTML = `${i + 1}. ${result.title}`
+    div.innerHTML = `${result.score} points by <a href=${byUrl}>${result.by}</a>
+      <a href=${timeUrl}>${postedElapsed}</a>
+      | <a href=${logInUrl}>hide</a> |
+      <a href=${commentsUrl}>${result.descendants} comments</a>`
+    append(p, div)
+    append(section, p)
+  })
+}
+
+getNews().then((result) => {
+  console.log(result)
+  let count = 0;
+  // const firstSet = result.filter((i, index) => (index < 30))
+  const firstSet = storiesArr(count, result)
+  count += 30;
+
+  for (let i = 0; i < firstSet.length; i++){
+    renderStory(firstSet[i], i)
+  }
+
+  window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+      // you're at the bottom of the page
+      const newSet = storiesArr(count, result)
+      count += 30;
+      for (let i = 0; i < newSet.length; i++) {
+        renderStory(newSet[i], i + count)
+      }
+    }
+  };
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
