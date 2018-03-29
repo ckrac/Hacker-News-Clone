4ea788925bcc03de90e07443e5feec69af01fb6c
vagrant <kennethracadio@hotmail.com>
2018-03-29 21:36:12 +0000

    Finished first set of styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -55,6 +55,7 @@ storiesArr = (count, result) => {
   return stories
 }
 
+
 domain_from_url = (url) => {
     let result;
     let match;
@@ -70,18 +71,8 @@ domain_from_url = (url) => {
 renderStory = (storyObj, i) => {
   getStory(storyObj).then((result) => {
     postedElapsed = timeDiff(result.time)
-
-    let domainUrl;
-    let match;
-    if (match = (result.url).match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
-        domainUrl = match[1]
-        if (match = domainUrl.match(/^[^\.]+\.(.+\..+)$/)) {
-            domainUrl = match[1]
-        }
-    }
     // don't know why i get an error here (Paused on promise rejection: Cannot read property 'match' of undefined)
-    // const domainUrl = domain_from_url(result.url)
-
+    const domainUrl = domain_from_url(result.url)
     const storyUrl = `${result.url}`
     const fromDomainUrl = `https://news.ycombinator.com/from?site=${domainUrl}`
 
@@ -96,7 +87,7 @@ renderStory = (storyObj, i) => {
     p.innerHTML = `<span class="top-story-index">${i + 1}.</span>
       <a href="#"><img src="https://news.ycombinator.com/grayarrow2x.gif" class="votearrow"></a>
       <span class="top-story-title"><a href=${storyUrl}>${result.title}</a>
-      <span class="top-story-domain">(<a href=${fromDomainUrl}>${domainUrl}</a>)</span>`
+      <span class="top-story-domain" id="top-story-domain">(<a href=${fromDomainUrl}>${domainUrl}</a>)</span>`
     div.innerHTML = `${result.score} points by
       <span class="bottom-story-a">
         <a href=${byUrl}>${result.by}</a>
@@ -124,10 +115,10 @@ getNews().then((result) => {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
       // you're at the bottom of the page
       const newSet = storiesArr(count, result)
-      count += 30;
       for (let i = 0; i < newSet.length; i++) {
         renderStory(newSet[i], count + i)
       }
+      count += 30;
     }
   };
 })
