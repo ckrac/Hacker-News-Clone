806ef83fc11f47f6a70df4bf110ae1df1fb07879
vagrant <kennethracadio@hotmail.com>
2018-04-02 18:52:38 +0000

    Sort first 30 stories

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,16 +11,16 @@ getNews = () => {
     })
 }
 
-getStory = (story) => {
-  const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
-  return fetch(storyUrl)
-    .then((resp) => {
-      return resp.json()
-    })
-    .then((json) => {
-      return json
-    })
-}
+// getStory = (story) => {
+//   const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
+//   return fetch(storyUrl)
+//     .then((resp) => {
+//       return resp.json()
+//     })
+//     .then((json) => {
+//       return json
+//     })
+// }
 
 createNode = (element) => {
   return document.createElement(element)
@@ -66,13 +66,106 @@ domain_from_url = (url) => {
     return result
 }
 
-renderStory = (storyObj, i) => {
-  getStory(storyObj).then((result) => {
+// renderStory = (storyObj, i, storiesToAppend) => {
+//   getStory(storyObj).then((result) => {
+//     postedElapsed = timeDiff(result.time)
+//     // don't know why i get an error here (Paused on promise rejection: Cannot read property 'match' of undefined)
+//     const domainUrl = domain_from_url(result.url)
+//     const storyUrl = `${result.url}`
+//     const fromDomainUrl = `https://news.ycombinator.com/from?site=${domainUrl}`
+
+//     const byUrl = `https://news.ycombinator.com/user?id=${result.by}`
+//     const timeUrl = `https://news.ycombinator.com/item?id=${result.id}`
+//     const logInUrl = `https://news.ycombinator.com/hide?id=${result.id}&goto=news`
+//     const commentsUrl = `https://news.ycombinator.com/item?id=${result.id}`
+//     let p = createNode("p")
+//     let div = createNode("div")
+//     p.className += "top-story"
+//     div.className += "bottom-story"
+//     p.innerHTML = `<span class="top-story-index">${i + 1}.</span>
+//       <a href="#"><img src="https://news.ycombinator.com/grayarrow2x.gif" class="votearrow"></a>
+//       <span class="top-story-title"><a href=${storyUrl}>${result.title}</a>
+//       <span class="top-story-domain" id="top-story-domain">(<a href=${fromDomainUrl}>${domainUrl}</a>)</span>`
+//     div.innerHTML = `${result.score} points by
+//       <span class="bottom-story-a">
+//         <a href=${byUrl}>${result.by}</a>
+//         <a href=${timeUrl}>${postedElapsed}</a>
+//         | <a href=${logInUrl}>hide</a> |
+//         <a href=${commentsUrl}>${result.descendants} comments</a>
+//       </span>`
+//     storiesToAppend.push({order: i, pElement: p, divElement: div})
+//     append(p, div)
+//     append(storyContainer, p)
+//   })
+// }
+
+generateUrls = (storiesArr) => {
+  const storyUrls = storiesArr.map(story => `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`)
+  return storyUrls
+}
+
+getStory = (url) => {
+  return fetch(url)
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((json) => {
+      return json
+    })
+}
+
+getStories = (urls) => {
+  const multipleFetch = urls.map((url,index) => getStory(url))
+  return multipleFetch
+}
+
+extractHostname = (url) => {
+  let hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+
+  if (url.indexOf("://") > -1) {
+      hostname = url.split('/')[2];
+  }
+  else {
+      hostname = url.split('/')[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+
+  return hostname;
+}
+
+extractRootDomain = (url) => {
+  let domain = extractHostname(url),
+      splitArr = domain.split('.'),
+      arrLen = splitArr.length;
+
+  //extracting the root domain here
+  //if there is a subdomain
+  if (arrLen > 2) {
+      domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+      //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+      if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+          //this is using a ccTLD
+          domain = splitArr[arrLen - 3] + '.' + domain;
+      }
+  }
+  return domain;
+}
+
+renderStory = (result, i) => {
     postedElapsed = timeDiff(result.time)
-    // don't know why i get an error here (Paused on promise rejection: Cannot read property 'match' of undefined)
-    const domainUrl = domain_from_url(result.url)
+    let domainUrl;
+    // check if their is an attached url from respons
+    if (result.url) {
+      domainUrl = extractRootDomain(result.url)
+    }
     const storyUrl = `${result.url}`
     const fromDomainUrl = `https://news.ycombinator.com/from?site=${domainUrl}`
+        // const fromDomainUrl = `#`
 
     const byUrl = `https://news.ycombinator.com/user?id=${result.by}`
     const timeUrl = `https://news.ycombinator.com/item?id=${result.id}`
@@ -82,10 +175,18 @@ renderStory = (storyObj, i) => {
     let div = createNode("div")
     p.className += "top-story"
     div.className += "bottom-story"
-    p.innerHTML = `<span class="top-story-index">${i + 1}.</span>
-      <a href="#"><img src="https://news.ycombinator.com/grayarrow2x.gif" class="votearrow"></a>
-      <span class="top-story-title"><a href=${storyUrl}>${result.title}</a>
-      <span class="top-story-domain" id="top-story-domain">(<a href=${fromDomainUrl}>${domainUrl}</a>)</span>`
+
+    if (domainUrl) {
+      p.innerHTML = `<span class="top-story-index">${i + 1}.</span>
+        <a href="#"><img src="https://news.ycombinator.com/grayarrow2x.gif" class="votearrow"></a>
+        <span class="top-story-title"><a href=${storyUrl}>${result.title}</a>
+        <span class="top-story-domain" id="top-story-domain">(<a href=${fromDomainUrl}>${domainUrl}</a>)</span>`
+    } else {
+      p.innerHTML = `<span class="top-story-index">${i + 1}.</span>
+        <a href="#"><img src="https://news.ycombinator.com/grayarrow2x.gif" class="votearrow"></a>
+        <span class="top-story-title"><a href=${storyUrl}>${result.title}</a>`
+    }
+
     div.innerHTML = `${result.score} points by
       <span class="bottom-story-a">
         <a href=${byUrl}>${result.by}</a>
@@ -93,28 +194,43 @@ renderStory = (storyObj, i) => {
         | <a href=${logInUrl}>hide</a> |
         <a href=${commentsUrl}>${result.descendants} comments</a>
       </span>`
+    // storiesToAppend.push({order: i, pElement: p, divElement: div})
     append(p, div)
     append(storyContainer, p)
-  })
 }
+
 
 getNews().then((result) => {
   let count = 0;
   // const firstSet = result.filter((i, index) => (index < 30))
+  const storiesToAppend =[]
   const firstSet = storiesArr(count, result)
+  const urlsFirstSet = generateUrls(firstSet)
+  console.log(urlsFirstSet)
+  const apiRequests = getStories(urlsFirstSet)
   count += 30;
 
-  for (let i = 0; i < firstSet.length; i++){
-    renderStory(firstSet[i], i)
-  }
+  Promise.all(apiRequests).then((result) => {
+    console.log("from promise",result)
+    console.log("0index", result[0])
+    for (let i = 0; i < apiRequests.length; i++){
+      renderStory(result[i], i)
+    }
+  })
+
+  // for (let i = 0; i < firstSet.length; i++){
+  //   renderStory(firstSet[i], i, storiesToAppend)
+  // }
+
+
 
   window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
       // you're at the bottom of the page
       const newSet = storiesArr(count, result)
-      for (let i = 0; i < newSet.length; i++) {
-        renderStory(newSet[i], count + i)
-      }
+      // for (let i = 0; i < newSet.length; i++) {
+      //   renderStory(newSet[i], count + i)
+      // }
       count += 30;
     }
   };
